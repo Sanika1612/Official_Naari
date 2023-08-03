754e2e09369e3a6cc350b5efaa54cb231f9455c5
Sanika1612 <117501602+Sanika1612@users.noreply.github.com>
2023-08-03 19:41:50 +0530

    files upload

---
+++ project/client/src/pages/landingPage/LandingPage.jsx
@@ -0,0 +1,15 @@
+
+import Hero from "../../components/Hero"
+import Navbar from "../../components/Navbar"
+import Feature from "../../components/Features"
+
+const LandingPage = () => {
+  return (
+    <div>
+        <Navbar />
+        <Hero />
+        <Feature />
+    </div>
+  )
+}
+export default LandingPage
